4eab40a9cc7f3d7bc18131331f053d9f6445d933
jaeyoolkim <s09c073@gmail.com>
2023-03-23 23:47:05 +0900

    # 서버 실행에 대한 코드

---
+++ node_js_lecture/server/server1.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+// 서버 실행 !! 
+const server = http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.write('<p>Hello server</p>');
+    res.end('<p>Hello ChestNut</p>');
+
+}).listen(8080);
+    // 프로세스 설정
+    // 포트번호 8080
+    // .listen(8080, () => {
+    //     console.log('Server Port 8080 Ready!!!');
+    // });
+
+// 서버 이벤트
+server.on('listening', () => {
+    console.log('Server Port 8080 Ready!!!');
+});
+
+// 에러 처리 
+server.on('error', (error) => {
+    console.error(error);
+});
